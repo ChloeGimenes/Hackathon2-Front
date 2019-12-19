a9803e5bbe22a1e3c1fabde46f7b1f078697248b
Maxime <maxime.lombardo@gmail.com>
2019-12-19 21:50:35 +0100

    correction dossier

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Cockpit from "./pages/Cockpit";
 import ErrorFound from "./pages/ErrorFound";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/ErrorFound" component={ErrorFound} />
-      <Route path="/Cockpit" component={Cockpit} />
+      <Route exact path='/' component={Home} />
+      <Route path='/ErrorFound' component={ErrorFound} />
+      <Route path='/Cockpit' component={Cockpit} />
     </Switch>
   );
 }
--- src/pages/Cockpit.jsx
+++ src/pages/Cockpit.jsx
@@ -1,23 +1,19 @@
 import React, { Component } from "react";
-import '../styles/Cockpit.css'
-
-
+import "../styles/Cockpit.css";
 
 class Cockpit extends Component {
- 
   render() {
     return (
-      
-  <div className="cockpit-content">   
-<video autoPlay muted loop id="myVideo">
-  <source src={require("../assets/cockpit.mp4")} type="video/mp4" />
-</video>
-<div className="cockpit-board">
-<div className="cockpit-board-image"></div>
-</div>
-</div>
-
+      <div className='cockpit-content'>
+        <video autoPlay muted loop id='myVideo'>
+          <source src={require("../assets/cockpit.mp4")} type='video/mp4' />
+        </video>
+        <div className='cockpit-board'>
+          <div className='cockpit-board-image'></div>
+        </div>
+      </div>
     );
-  }}
+  }
+}
 
 export default Cockpit;
