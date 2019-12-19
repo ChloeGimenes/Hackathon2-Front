67f1a1193d44f6d861ba81511d2da2f9b8a894d1
Chloe Gimenes <chloegimenes@gmail.com>
2019-12-19 11:50:43 +0100

    commit de depart

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 import React from "react";
 
-import "./style.scss";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div class="container">
-        <div class="follow">
-          <div class="icon first"></div>
-          <div class="icon"></div>
-          <div class="icon"></div>
-          <div class="icon last"></div>
-          <div class="label">MAX</div>
-        </div>
-        <div class="share">
-          <div class="icon first"></div>
-
-          <div class="label">MILAN</div>
-        </div>
-        <button class="try">
-          <span class="text">CHLOE</span>
-        </button>
-        <button class="buy">
-          <span class="price">
-            <small></small>flow
-          </span>
-          <span class="text">FLO</span>
-        </button>
+    <div>
+      <div class="stars"></div>
+      <div class="twinkling">
+        <iframe
+          src="https://giphy.com/embed/3Y1nbBv3BPxRe"
+          width="480"
+          height="275"
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+        ></iframe>
       </div>
+
+      <div class="clouds"></div>
     </div>
   );
 }
